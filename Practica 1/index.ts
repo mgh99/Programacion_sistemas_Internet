1b62f7d921cca3db5228e156dd90059405ebc4e4
María González Herrero <43043718+mgh99@users.noreply.github.com>
2021-10-07 21:40:08 +0200

    Update index.ts

--- Practica 1/index.ts
+++ Practica 1/index.ts
@@ -9,7 +9,6 @@ const variable ={
 
         f:3,
         c:{
-
             j:[{a:1,b:2},3,{c:1,a:2}]
         }
     }
@@ -17,22 +16,24 @@ const variable ={
 
 const toString=(parametro:any):String=>{
 
-        var retorno:String="";
+        var retorno:String="";// va a ser la variable que se va a declarar cada vez que entremos y se encadenará con sus recursivos
         if(!Array.isArray(parametro) && typeof(parametro) !== "object") {
             // si no es un array y tampoco es un objeto
-            if(typeof(parametro)=="string"){
+            if(typeof(parametro)=="string"){//si es un string devuelvo la palabra entr ""
                 return ("\""+String(parametro)+"\"");
-            }else if(typeof(parametro)=="number"){
+            }else if(typeof(parametro)=="number"){// si es un numero solo devuelvo el numero pasado a string
+                return String(parametro);
+            }else if(typeof(parametro)=="boolean"){
                 return String(parametro);
             }
-        }else {// un tipo complejo , tipo objeto creo 
+        }else {// un tipo complejo
             if(Array.isArray(parametro)){// si es una array
-                retorno+="["
-                for (var _i = 0; _i < parametro.length; _i++) {
-                    if(_i==parametro.length-1){
-                        retorno+=String(toString(parametro[_i]))
+                retorno+="["// indicamos que es una array
+                for (var i = 0; i < parametro.length; i++) {
+                    if(i==parametro.length-1){
+                        retorno+=String(toString(parametro[i]))
                     }else{
-                        retorno+=(String(toString(parametro[_i]))+",");
+                        retorno+=(String(toString(parametro[i]))+",");
                     }
                 }
                 retorno+="]";
@@ -57,5 +58,5 @@ console.log(toString(variable));
 if(toString(variable)==JSON.stringify(variable)){
     console.log("Ejercicio 1 funciona");
 }else{
-    console.log("Jodete")
+    console.log("No funciona");
 }
